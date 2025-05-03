48afc152c427d692f233b8638c819d198bc1d5c0
yashmahto <yashmahto244@gmail.com>
2025-05-03 17:46:59 +0530

    Optimize the Routes

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import authRoutes from "./routes/auth.route.js"
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT,() => { 
+  console.log(`Server is running on port ${PORT}`);
+})
