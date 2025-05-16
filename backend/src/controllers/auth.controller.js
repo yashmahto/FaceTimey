afd9a10662f81b4a11ee2f2d5a7f8dbcf0726963
yashmahto <yashmahto244@gmail.com>
2025-05-16 19:27:23 +0530

    Implement logout controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -69,7 +69,7 @@ export async function login(req,res){
       const isPasswordCorrect = await user.matchPassword(password);
       if(!isPasswordCorrect) return res.status(401).json({message : "Invalid email or password"});
 
-          const token = jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY,{
+          const token = jwt.sign({userId:user._id},process.env.JWT_SECRET_KEY,{
             expiresIn: "7d"
           })
 
@@ -90,5 +90,6 @@ export async function login(req,res){
 }
 
 export async function logout(req,res){
-    res.send("Logout Route");
+    res.clearCookie("jwt")
+    res.status(200).json({success : true , message : "Logout sucessfull"});
 }
