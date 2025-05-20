53e3e805fd24311f522be090a84babc59d83b3e7
yashmahto <yashmahto244@gmail.com>
2025-05-20 13:38:21 +0530

    Complete User Routes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -113,9 +113,42 @@ export async function acceptFriendRequest(req,res) {
         });
 
         res.status(200).json({message : "Friend request accepted"});
-        
+
     } catch (error) {
         console.error("Error in  AcceptFriendRequset Controller", error.message);
+        res.status(500).json({message : "Internal Server Error"});
+    }
+}
+
+export async function getFriendRequests(req,res) {
+    try {
+        const incomingReqs = await FriendRequest.find({
+            recipient : req.user.id ,
+            status : "pending",
+        }).populate("sender" , "fullName profilePic nativeLanguage  learningLanguage");
+
+        const acceptedReqs = await FriendRequest.find({
+            sender : req.user.id,
+            status : "accepted",
+        }).populate("recipient" , "fullName profilePic");
+
+        res.status(200).json({incomingReqs , acceptedReqs});
+    } catch (error) {
+        console.log("Error on getPendingFriendRequest Controller", error.message);
+        res.status(500).json({message : "Internal Server Error"});
+    }
+}
+
+export async function getOutgoingFriendReqs(req , res) { 
+    try {
+        const outgoingRequests = await FriendRequest.find({
+            sender : req.user.id,
+            status : "pending",
+        }).populate("recipient" , "fullName profilePic nativeLanguage learningLanguage")
+
+        res.status(200).json(outgoingRequests);
+    } catch (error) {
+        console.log("Error in getOutgoingFriendReqs controller" , error.message);
         res.status(500).json({message : "Internal Server Error"});
     }
 }
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { acceptFriendRequest, getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
+import { acceptFriendRequest, getFriendRequests, getMyFriends, getOutgoingFriendReqs, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -11,7 +11,10 @@ router.get("/", getRecommendedUsers);
 router.get("/friends",getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
-router.post("friend-request/:id/accept" , acceptFriendRequest);
+router.put("/friend-request/:id/accept" , acceptFriendRequest);
+
+router.get("/friend-requests" , getFriendRequests);
+router.get("/outgoing-friend-requests" , getOutgoingFriendReqs);
 
 export default router;
 
