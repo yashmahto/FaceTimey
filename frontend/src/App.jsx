245f0a3bbdc96da08ebd2064c9e16cb1d3eddda5
yashmahto <yashmahto244@gmail.com>
2025-05-23 13:56:45 +0530

    SignUp Page Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes , Route } from "react-router"
+import { Routes , Route, Navigate } from "react-router"
 
 import HomePage from "./pages/HomePage.jsx"
 import SignUpPage from "./pages/SignUpPage.jsx"
@@ -15,18 +15,17 @@ import { axiosInstance } from "./lib/axios.js"
 
 const App = () => {
   
-const {data , isLoading , error} = useQuery({
-  queryKey:["todos"],
-
-
+const {data : authData , isLoading , error} = useQuery({
+  queryKey:["authUser"],
   queryFn : async() => {
 
-    const res = await axiosInstance.get("http://localhost:5001/api/auth/me");
+    const res = await axiosInstance.get("/auth/me");
     return res.data;
   },
+  retry: false,
 });
 
-console.log(data);
+const authUser = authData?.user
 
 
   
@@ -35,13 +34,13 @@ console.log(data);
     
       <Routes>
         
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/signup" element={<SignUpPage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/notification" element={<NotificationPage/>} />
-        <Route path="/call" element={<CallPage/>} />
-        <Route path="/chat" element={<ChatPage/>} />
-        <Route path="/onboarding" element={<OnboardingPage/>} />
+        <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/" /> } />
+        <Route path="/login" element={!authUser ? <LoginPage/>  : <Navigate to="/" /> } />
+        <Route path="/notification" element={authUser ? <NotificationPage/> : <Navigate to="/login" />} />
+        <Route path="/call" element={authUser ? <CallPage/> : <Navigate to="/login" />}/>
+        <Route path="/chat" element={authUser ? <ChatPage/> : <Navigate to="/login" /> }/>
+        <Route path="/onboarding" element={authUser ? <OnboardingPage/> : <Navigate to="/login" />} />
       </Routes>
       
       <Toaster />
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState } from "react"
+import {ShipWheelIcon} from "lucide-react";
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
+  const [signupData , setSignupData] = useState({
+    fullNamee : "",
+    email : "",
+    password : "",
+  });
+
+  const handleSignup = (e) => {
+    e.preventDefault()
+  }
+
+  return <div className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8" data-theme="forest">
+     <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 
+     rounded-xl shadow-lg overflow-hidden">
+
+     {/* Sign up form - left Side */}
+
+     <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col ">
+      <div className="mb-4 flex items-center justify-start gap-2">
+        <ShipWheelIcon className="size-9 text-primary" />
+        <span>
+          FaceTimey
+        </span>
+      </div>
+
+     </div>
+     </div>
+      
+      </div>
+  
 }
 
-export default SignUpPage
+export default SignUpPage;
